0e15f534947f6b080267aa2ec3419bb68f64201a
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2022-12-01 14:35:19 -0700

    simplify day 1 a little bit

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -10,20 +10,12 @@ fn main() -> Result<()> {
     let mut values = contents
         .trim()
         .split("\n\n")
-        .map(|x| {
-            x.split("\n")
-                .map(|y| y.parse::<i64>().unwrap())
-                .collect::<Vec<i64>>()
-                .into_iter()
-                .sum()
-        })
+        .map(|x| x.split("\n").map(|y| y.parse::<i64>().unwrap()).sum())
         .collect::<Vec<i64>>();
 
     values.sort();
 
-    let max_val = values.iter().max().unwrap();
-
-    dbg!(max_val);
+    dbg!(values[values.len() - 1]);
 
     dbg!(values[values.len() - 3..].iter().sum::<i64>());
 
